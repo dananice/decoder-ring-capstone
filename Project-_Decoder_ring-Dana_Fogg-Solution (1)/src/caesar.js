62916e899fee05363571bd58ac254ffe742912ae
Dana Fogg <102395872+dananice@users.noreply.github.com>
2022-05-07 18:16:08 -0400

    Update caesar.js

--- Project-_Decoder_ring-Dana_Fogg-Solution (1)/src/caesar.js
+++ Project-_Decoder_ring-Dana_Fogg-Solution (1)/src/caesar.js
@@ -5,9 +5,30 @@
 
 const caesarModule = (function () {
   // you can add any code you want within this function scope
+      const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
   function caesar(input, shift, encode = true) {
     // your solution code here
+    input = input.toLowerCase();
+      if (shift === 0 || shift < -25 || shift > 25) {
+        return false;
+      } //error handling returning false
+      if (!encode) {
+        shift = -shift;
+      }
+    for (let i = 0; i < input.length; i++) {
+        let character = input.charCodeAt(i);
+        if (character < 97 || character > 122) {
+        message.push(String.fromCharCode(character)) 
+        } else {
+          //encoding a message
+        character += shift;
+        if (character > 96 && character < 123)message.push(String.fromCharCode(character));
+        if (character < 97)message.push(String.fromCharCode(123 -(97 - character))); 
+        if (character > 122)message.push(String.fromCharCode(96 +(character -122)));
+        }
+      } //decoding a message
+      return message.join("");
   }
 
   return {
