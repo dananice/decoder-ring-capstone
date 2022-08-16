5607a17e34b99c34f4f3455b9fa71383eafc548b
Dana Fogg <102395872+dananice@users.noreply.github.com>
2022-08-15 22:27:34 -0400

    Update substitution.js

--- Project-_Decoder_ring-Dana_Fogg-Solution (1)/scripts/substitution.js
+++ Project-_Decoder_ring-Dana_Fogg-Solution (1)/scripts/substitution.js
@@ -1,25 +1,61 @@
-function substitutionRenderer() {
-  const form = document.querySelector("#substitution");
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const input = event.target["substitution-input"].value;
-    const direction = event.target["substitution-options"].value;
-    const alphabet = event.target["substitution-alphabet"].value;
-    const result =
-      direction === "encode"
-        ? substitutionModule.substitution(input, alphabet)
-        : substitutionModule.substitution(input, alphabet, false);
-
-    const alert = document.querySelector("#substitution-alert");
-    if (result) {
-      alert.classList.add("d-none");
-      const output = document.querySelector("#substitution-output");
-      output.innerHTML = result;
-    } else {
-      alert.classList.remove("d-none");
+const substitutionModule = (function () {
+    // you can add any code you want within this function scope
+    function substitution(input, alphabet, encode = true) {
+      // your solution code here
+      if (!alphabet || alphabet.length !== 26) return false;
+      const lowerCaseInput = input.toLowerCase();
+      const trueAlphabet = "abcdefghijklmnopqrstuvwxyz";
+      const alphabetArray = alphabet.split('')
+    
+    // const string = 'word';
+    // string.split('');
+    // Result : ['w', 'o', 'r', 'd']
+      let resultFinal = [];
+      // [
+      //   '.', 'w', 'a', 'e', 's', 'z',
+      //   'r', 'd', 'x', 't', 'f', 'c',
+      //   'y', 'g', 'v', 'u', 'h', 'b',
+      //   'i', 'j', 'n', 'o', 'k', 'm',
+      //   'p', 'l'
+      // ]
+      // you can use .indexOf() and .lastIndexOf() to determine if an index is repeated. Meaning, if the first occurrence of the character is also the last occurrence, then you know it doesn't repeat. If not true, then it does repeat.
+  
+      for (let i=0; i<alphabetArray.length; i++){
+        if ( alphabetArray.indexOf(alphabetArray[i]) !== alphabetArray.lastIndexOf(alphabetArray[i]) ){
+          return false;
+        }
+      }
+      //space
+      for(let i =0; i<lowerCaseInput.length; i++){
+        if(lowerCaseInput[i]=== " "){
+          resultFinal.push(" ")
+        }
+  
+  // encode
+  // substitution("message", "plmoknijbuhvygctfxrdzeswaq") => "ykrrpik")
+  for(let j=0; j<trueAlphabet.length; j++){
+    if(encode && trueAlphabet[j]===lowerCaseInput[i]){
+      resultFinal.push(alphabet[j])
+      
     }
-  });
-}
-
-document.addEventListener("DOMContentLoaded", substitutionRenderer);
+  }
+  
+  
+  
+  //decode//
+  //substitution("y&ii$r&", "$wae&zrdxtfcygvuhbijnokmpl", false); //> "message"    
+  for(let j =0; j<alphabet.length; j++){
+      if(encode ===false && alphabet[j] === lowerCaseInput[i]){
+        resultFinal.push(trueAlphabet[j])
+       }
+      }
+      }
+  
+    return resultFinal.join('')
+    }
+    return {
+      substitution,
+    };
+  })();
+  module.exports = { substitution: substitutionModule.substitution };
+  ;
